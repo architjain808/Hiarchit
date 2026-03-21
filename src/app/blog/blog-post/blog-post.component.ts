0c0c5903f92448771d6185b7d2ee805099bf27e2
architjain808 <architjain808@gmail.com>
2026-03-21 15:28:46 +0530

    UI update

--- src/app/blog/blog-post/blog-post.component.ts
+++ src/app/blog/blog-post/blog-post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, AfterViewInit, inject, PLATFORM_ID, ElementRef } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterLink, ActivatedRoute } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
@@ -12,13 +13,16 @@ import { getPostBySlug, BlogPost } from '../posts.registry';
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.scss',
 })
-export class BlogPostComponent implements OnInit {
+export class BlogPostComponent implements OnInit, AfterViewInit {
   private route = inject(ActivatedRoute);
   private seoService = inject(SeoService);
   private sanitizer = inject(DomSanitizer);
+  private platformId = inject(PLATFORM_ID);
+  private el = inject(ElementRef);
 
   post: BlogPost | undefined;
   safeContent: SafeHtml = '';
+  linkCopied = false;
 
   ngOnInit(): void {
     const slug = this.route.snapshot.paramMap.get('slug') ?? '';
@@ -28,5 +32,22 @@ export class BlogPostComponent implements OnInit {
       this.safeContent = this.sanitizer.bypassSecurityTrustHtml(this.post.content);
       this.seoService.setBlogPostSEO(this.post.title, this.post.excerpt, slug);
     }
+  }
+
+  ngAfterViewInit(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+    const links = this.el.nativeElement.querySelectorAll('.prose a');
+    links.forEach((a: HTMLAnchorElement) => {
+      a.setAttribute('target', '_blank');
+      a.setAttribute('rel', 'noopener noreferrer');
+    });
+  }
+
+  copyLink(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+    navigator.clipboard.writeText(window.location.href).then(() => {
+      this.linkCopied = true;
+      setTimeout(() => (this.linkCopied = false), 2000);
+    });
   }
 }
